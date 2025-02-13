46ff7bab39870261b4fb823aebc7b259dd9a06df
HannanMaxwell <__max__@live.com>
2025-02-13 06:14:30 -0600

    Applied interest to the account balances based on the amount.

--- interest_update.py
+++ interest_update.py
@@ -2,7 +2,7 @@
 
 """
 
-import pprint
+from pprint import pprint
 
 customer_balances = {}
 
@@ -10,15 +10,19 @@ with open("account_balances.txt", "r") as file:
     for line in file:
         account_number, account_balance = line.strip().split("|")
         customer_balances[account_number] = float(account_balance)
-        print()
-        
-pprint.pp(customer_balances)
 
-#     for account_number, account_balance in content:
-#     customer_balances = 
+pprint(customer_balances)
 
-# file = open("account_balances.txt", r)
-# content = file.read().split("|")
+for account_number, account_balance in customer_balances.items():
+    if account_balance < 1000 and account_balance >= 0:
+        customer_balances[account_number] = account_balance + (account_balance * 0.01)/12
+    elif account_balance > 1000 and account_balance < 5000:
+        customer_balances[account_number] = account_balance + (account_balance * 0.025)/12
+    elif account_balance > 5000:
+        customer_balances[account_number] = account_balance + (account_balance * 0.05)/12
+    else:
+        customer_balances[account_number] = account_balance + (account_balance * 0.1)/12
 
-# print(type(customer_balances))
+
+pprint(customer_balances)
 
